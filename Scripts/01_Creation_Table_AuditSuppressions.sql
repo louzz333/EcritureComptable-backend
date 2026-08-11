489a453a70c2f18a0ca3bd9b4ff9ff540a98a2c9
louzz333 <melmossadak@gmail.com>
2026-08-11 01:31:09 +0100

    Ajout script SQL pour AuditSuppressions, statut fonctionnel, colonne EtatComptabilisation

---
+++ Scripts/01_Creation_Table_AuditSuppressions.sql
@@ -0,0 +1,21 @@
+﻿-- ============================================
+ Script : Création de la table AuditSuppressions
+ Objectif : historiser les suppressions d'écritures comptables
+-- ============================================
+
+CREATE TABLE [dbo].[AuditSuppressions](
+    [Id] [int] IDENTITY(1,1) NOT NULL,
+    [NumeroEcriture] [numeric](10, 0) NULL,
+    [DateEcriture] [datetime] NULL,
+    [JournalEcriture] [nvarchar](100) NULL,
+    [CompteEcriture] [nvarchar](100) NULL,
+    [MontantEcriture] [numeric](13, 4) NOT NULL,
+    [SensEcriture] [nvarchar](1) NULL,
+    [ReferenceEcriture] [nvarchar](100) NULL,
+    [DeviseEcriture] [nvarchar](100) NULL,
+    [Libelle] [nvarchar](500) NOT NULL,
+    [DateSuppression] [datetime] NOT NULL,
+    [Motif] [nvarchar](500) NOT NULL,
+    CONSTRAINT [PK_AuditSuppressions] PRIMARY KEY ([Id])
+) ON [PRIMARY]
+GO
